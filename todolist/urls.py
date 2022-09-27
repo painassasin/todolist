347f7d0794db6333dd1ebc9100dc304160f4d71b
painassasin <painassasin@icloud.com>
2022-09-27 19:45:21 +0500

    social auth

--- todolist/urls.py
+++ todolist/urls.py
@@ -5,6 +5,7 @@ from todolist.core.views import health_check
 
 urlpatterns = [
     path('core/', include('todolist.core.urls')),
+    path('oauth/', include('social_django.urls', namespace='social')),
     path('ping/', health_check, name='health-check'),
     path('admin/', admin.site.urls),
 ]
